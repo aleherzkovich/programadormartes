0c37c435be237298f08b194a2a580e96fc5a4925
Alexis Herzkovich <aleherzkovich@outlook.com>
2022-07-18 18:42:22 -0300

    probando otras maneras

--- Tarea M1U4/tarea4.js
+++ Tarea M1U4/tarea4.js
@@ -1,24 +1,39 @@
 var nombre;
 var distancia;
 
+
 nombre = prompt('Ingrese su nombre', '')
 distancia = prompt  ('Hola '+ nombre+ ' soy un bot hecho por JavaScript y necesito hacerte una simple pregunta ¿Cuántos metros crees que hay entre el Obelisco de Buenos Aires y tu vivienda?', '')
 
  
-distancia=parseInt(distancia)
+distancia=parseInt(distancia)0
 //if(distancia<100 >=1) {document.write ('hola soy un intento mas')}
 
 if(distancia<1000){document.write(distancia + ' metros no es una gran distancia,si deseas visitar el Obelisco te recomiendo ir a pie, eso es todo, haz click en "aceptar" para continuar        ');}
 if(distancia>1000){document.write(distancia + ' metros es mucho, te recomiendo ir en bici      ');}
 
-var num = ['2','1','4','15','3','9','5','12','10','6','14','8','11','13','7','17','16']
-num=parseInt(num)
 
-while(num<=17){
-    document.write(num);
-    document.write('<br>');
-    num=num+1;
-  }
+
+
+var num;
+
+var num = ['20','110','54','15','3','9','75','12','167','10','26','17','14','8','121','83','77','160'];
+//num= parseInt(num)
+
+
+//for( var i=0; i< num.length; i++){document.write('hay'+ num[i])}
+
+//document.write('el numero mas alto es '+ num[])
+
+
+
+
+//document.write(num)
+// while(num<=167){
+//     document.write(num);
+//     document.write('<br>');
+//     num=num+1; }
+ 
 //if(distancia<5000 >=1000){document.write (distancia+ ' funciona');}
 
 //un ejemplo if (nota>=4){document.write(nombre+' esta aprobado con un '+ nota);}else{document.write(nombre+' esta desaprobado con un '+ nota);}
